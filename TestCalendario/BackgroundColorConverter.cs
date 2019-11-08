a9dbf009c39c3adc92f274590fb7132aa989106d
David <david.porqueras@expertus.es>
2019-11-08 14:58:35 +0100

    first commit

---
+++ TestCalendario/BackgroundColorConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestCalendario
+{
+    public class CalendarDayBackgroundColorConverter : IMultiValueConverter
+    {
+        public CalendarDayBackgroundColorConverter()
+        {
+
+        }
+
+        #region IMultiValueConverter Members
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var dia = values[0] as DateTime?;
+            var control = values[1] as CustomCalendar;
+            var col = control.Days[dia.Value.Day - 1];
+
+            if (control.DisplayDate.Month == dia.Value.Month)
+            {
+                return col;
+            }
+
+            return new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return new object[0];
+        }
+
+        #endregion
+    }
+}
